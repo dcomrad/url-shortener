b7ed03a7e2b9f97c83c5ed3825a84be9929c6327
Aleksandr Bondarenko <dcomrad@gmail.com>
2023-04-16 02:02:29 +0200

    added index view and migrations

--- yacut/views.py
+++ yacut/views.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 from . import app
 
+from .forms import URLMapForm
+from .utils import get_unique_short_id
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = URLMapForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        short_id = form.custom_id.data or get_unique_short_id()
+        return render_template('index.html', form=form, short_id=short_id)
+    return render_template('index.html', form=form)
