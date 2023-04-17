4f3722b86e538ad414ed96cdd5475804f1c5d762
Aleksandr Bondarenko <dcomrad@gmail.com>
2023-04-17 02:01:55 +0200

    Fighting with the tests

--- settings.py
+++ settings.py
@@ -1,13 +1,9 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = True if os.getenv('DEBUG_MODE', 'False') == 'True' else False
     SQLALCHEMY_ECHO = True if os.getenv('DEBUG_MODE', 'False') == 'True' else False
     JSON_AS_ASCII = False
