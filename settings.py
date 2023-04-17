f9d0a4df1ca728baa0bb0facacf38da1090aecc2
Aleksandr Bondarenko <dcomrad@gmail.com>
2023-04-17 02:04:12 +0200

    Fighting with the tests

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True if os.getenv('DEBUG_MODE', 'False') == 'True' else False
     SQLALCHEMY_ECHO = True if os.getenv('DEBUG_MODE', 'False') == 'True' else False
     JSON_AS_ASCII = False
