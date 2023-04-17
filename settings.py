148aa14b4d3553c1df6a2154294d61adf156c7f6
Aleksandr Bondarenko <dcomrad@gmail.com>
2023-04-17 02:08:34 +0200

    Fighting with the tests

--- settings.py
+++ settings.py
@@ -3,7 +3,12 @@ import os
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    # При замене на os.getenv('DATABASE_URI') стабильно получаю ошибку
+    # "Проверьте, что конфигурационному ключу SQLALCHEMY_DATABASE_URI присвоено
+    # значение с настройками для подключения базы данных" при прохождении
+    # тестов на платформе. Локальные тесты проходят без проблем!
+    # При этом, тест test_config в test_config.py простой, как две копейки
     SQLALCHEMY_TRACK_MODIFICATIONS = True if os.getenv('DEBUG_MODE', 'False') == 'True' else False
     SQLALCHEMY_ECHO = True if os.getenv('DEBUG_MODE', 'False') == 'True' else False
     JSON_AS_ASCII = False
